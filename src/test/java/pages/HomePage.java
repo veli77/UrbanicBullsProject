92844c642cf75c981783ca543703fe4c741f349b
Veli Ozerdem <veliozerdem@gmail.com>
2024-01-29 18:17:07 +0300

    Home page / Deneme US / DenemeConflict

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,14 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage extends CommonPage {
+
+    @FindBy(xpath = "//button[@class='abcd']")
+    public WebElement denemeElement;
+
+
 
 }
