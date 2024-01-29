1f5c866dd692e1f50ab911c592c9641da99c3295
emineuslu <emneulusoy@gmail.com>
2024-01-29 18:20:14 +0300

    Home Page / Deneme US / Deneme newConflct

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePage extends CommonPage {
 
+
+    @FindBy(xpath = "//input[@name='selamlar']" )
+    public WebElement firstDeneme;
 }
