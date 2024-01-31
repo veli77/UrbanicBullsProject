4534d73594e74ff8baa047f70c6a874a4cad578c
EsraBaldiz <esracagan85@gmail.com>
2024-01-31 18:26:18 +0100

    us26

---
+++ src/test/java/stepDefinitions/US_26.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.CommonPage;
+import utilities.Driver;
+
+public class US_26 extends CommonPage {
+    Driver driver;
+    @Given("User goes to homepage")
+    public void userGoesToHomepage() {
+
+    }
+
+    @When("User click to Terms Of Service")
+    public void userClickToTermsOfService() {
+    }
+
+    @Then("User should be able to see relevant page")
+    public void userShouldBeAbleToSeeRelevantPage() {
+    }
+}
---
+++ src/test/resources/features/uiFeatures/homePage/1_BU-54.feature
@@ -0,0 +1,10 @@
+@BU-55
+Feature: Test Execution Of_US_026 
+
+	#Terms Of Service should be clickable and relevant page should be visible
+	@TEST_BU-54
+	Scenario: US_026 - TC01_User should be able to perform various operations on homepage
+		Given User goes to homepage
+		When User click to Terms Of Service
+		Then User should be able to see relevant page
+		
