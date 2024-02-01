17f63ed7a54a770443ad1e2fdc0b3e9490f60423
EsraBaldiz <esracagan85@gmail.com>
2024-02-01 16:57:22 +0100

    conflitcozuldu

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,12 +5,6 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends CommonPage {
 
-    @FindBy(xpath = "//button[@class='abcde']")
-    public WebElement denemeElement;
-
-    @FindBy(xpath = "//input[@name='selamlar']" )
-    public WebElement firstDeneme;
-
     @FindBy(xpath = "//li/a[.='contact us'][@href='/contact-us']")
     public WebElement ContactUsBtnAtBotton;
 
