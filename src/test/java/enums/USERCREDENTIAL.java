f0530b50e9ea8bb3f4c704ce67e68f5609752e83
EsraBaldiz <esracagan85@gmail.com>
2024-02-01 21:04:15 +0100

    EnamLoginOlusturuldu

---
+++ src/test/java/enums/USERCREDENTIAL.java
@@ -0,0 +1,29 @@
+package enums;
+
+public enum USERCREDENTIAL {
+    USER1("urbanicfarm2@gmail.com","Urbanicfarm2/"),
+    USER2("urbanicfarm2@yopmail.com", "Urbanicfarm2/"),
+    USER3("seller_urban@mailsac.com","VHt*zzt*wQNu6XS"),
+    USER4("buyer_urban@mailsac.com","VHt*zzt*wQNu6XS");
+
+
+
+
+    private final String username;
+    private final String password;
+
+    USERCREDENTIAL(String username, String password) {
+        this.username = username;
+        this.password=password;
+    }
+    public String getUsername(){
+        return getUsername();
+
+    }
+
+    public String getPassword(){
+     return getPassword();
+    }
+
+
+}
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,31 @@
 package pages;
 
+import enums.URLOGIN;
+import enums.USERCREDENTIAL;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static enums.URLOGIN.LOGIN_URL;
+import static stepDefinitions.Hooks.driver;
+
 public class LoginPage extends CommonPage{
+
+    @FindBy(xpath = "//input[@name='email']")
+    public WebElement input_email;
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement input_password;
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submit_button;
+
+    public void validCredential(){
+        driver.get(LOGIN_URL.getLink());
+        input_email.sendKeys(USERCREDENTIAL.USER1.getUsername());
+        input_password.sendKeys(USERCREDENTIAL.USER1.getPassword());
+        submit_button.click();
+
+
+
+    }
+
+
 }
