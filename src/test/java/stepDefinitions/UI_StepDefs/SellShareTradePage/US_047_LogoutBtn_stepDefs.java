d85ccb89bc154d85c81f3d9580eee5dd9f97d808
emineuslu <emneulusoy@gmail.com>
2024-02-04 05:47:46 +0300

    Framework updated and US_16 added

---
+++ src/test/java/stepDefinitions/UI_StepDefs/SellShareTradePage/US_047_LogoutBtn_stepDefs.java
@@ -0,0 +1,45 @@
+package stepDefinitions.UI_StepDefs.SellShareTradePage;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.CommonPage;
+import utilities.JSUtils;
+import utilities.ReusableMethods;
+
+public class US_047_LogoutBtn_stepDefs extends CommonPage {
+
+    @When("User clicks on Login module")
+    public void userClicksOnLoginModule() {
+        JSUtils.clickElementByJS(getHomePage().LoginBtnAtButton);
+    }
+
+
+    @And("User logs in")
+    public void userLogsIn() {
+
+        getLoginPage().login();
+    }
+
+
+    @When("User clicks on Sell-Share-Trade module")
+    public void user_clicks_on_sell_share_trade_module() throws InterruptedException {
+        JSUtils.clickElementByJS(getSellShareTradePage().Sell_Share_TradeBtn);
+        Thread.sleep(3000);
+
+    }
+
+
+    @When("User clicks on Logout module")
+    public void user_clicks_on_logout_module() {
+         ReusableMethods.waitForClickablility(getSellShareTradePage().LogoutBtn_SellShareTradePage,3).click();
+    }
+
+    @Then("Verifies that the user has returned to the home page")
+    public void verifies_that_the_user_has_returned_to_the_home_page() {
+        getSellShareTradePage().verifyHomePage();
+    }
+
+
+
+}
