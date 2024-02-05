3c4c077ef62c04b263227577b62e4d8fdb8d490d
abraham907 <ersinibrahimoral@gmail.com>
2024-02-05 14:29:02 +0100

    changes01_05.02

--- src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_42_GoToUserAccountPage_stepDefs.java
+++ src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_42_GoToUserAccountPage_stepDefs.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 import static stepDefinitions.Hooks.driver;
 
-public class US_42_GoToUserAccountPage_stepDefs {
-    CommonPage commonPage= new CommonPage();
+public class US_42_GoToUserAccountPage_stepDefs extends CommonPage{
+
 
     @Then("The user should see leftside tabs")
     public void theUserShouldSeeLeftsideTabs(DataTable dataTable) {
@@ -21,9 +21,9 @@ public class US_42_GoToUserAccountPage_stepDefs {
         Assert.assertTrue(driver.getCurrentUrl().contains("account"));
 
         List<String> tabs = new ArrayList<>();
-        for (int i = 0; i <commonPage.getAccountPage().accountPageLeftsidesTabs.size(); i++) {
+        for (int i = 0; i <getAccountPage().accountPageLeftsidesTabs.size(); i++) {
 
-            tabs.add(commonPage.getAccountPage().accountPageLeftsidesTabs.get(i).getText());
+            tabs.add(getAccountPage().accountPageLeftsidesTabs.get(i).getText());
         }
 
         for (String tab : tabsNames) {
