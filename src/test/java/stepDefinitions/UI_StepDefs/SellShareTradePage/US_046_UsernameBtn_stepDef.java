65ad79a28877c374e885dc524797a1f6fe2e02d2
umtdrk <umit_dirik@hotmail.com>
2024-02-08 22:40:03 +0300

    us_046 güncelleme

--- src/test/java/stepDefinitions/UI_StepDefs/SellShareTradePage/US_046_UsernameBtn_stepDef.java
+++ src/test/java/stepDefinitions/UI_StepDefs/SellShareTradePage/US_046_UsernameBtn_stepDef.java
@@ -1,7 +1,9 @@
 package stepDefinitions.UI_StepDefs.SellShareTradePage;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import pages.CommonPage;
@@ -10,16 +12,14 @@ import utilities.JSUtils;
 import static stepDefinitions.Hooks.driver;
 
 public class US_046_UsernameBtn_stepDef extends CommonPage {
-    @When("User clicks on Username button")
+    @Then("User clicks on Username button")
     public void userClicksOnUsernameButton() {
-
-        System.out.println(driver.getTitle());
-        JSUtils.clickElementByJS(getSellShareTradePage().usernameBtn);
-//        getSellShareTradePage().userNameBtnClick();
+        getSellShareTradePage().userNameBtnClick();
     }
 
-    @And("User goes to Account page")
+    @And("User verifies that the Account page has been opened")
     public void userGoesToAccountPage() {
-        System.out.println(driver.getTitle());
+        String expectedUrl="https://test.urbanicfarm.com/account/home";
+        Assert.assertEquals(expectedUrl,driver.getCurrentUrl());
     }
 }
