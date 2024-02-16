0c0aca63f2e91cde8da392a47da0d992a3539035
abraham907 <ersinibrahimoral@gmail.com>
2024-02-16 19:51:26 +0100

    change01 16.02

---
+++ src/test/java/stepDefinitions/UI_StepDefs/AccountHubPage/US_056_googleMapsFunctions_stepDefs.java
@@ -0,0 +1,27 @@
+package stepDefinitions.UI_StepDefs.AccountHubPage;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_056_googleMapsFunctions_stepDefs {
+    @When("User click to Address button, user can see related page")
+    public void userClickToAddressButtonUserCanSeeRelatedPage() {
+    }
+
+    @And("Zoom buttons should be clickable.")
+    public void zoomButtonsShouldBeClickable() {
+    }
+
+    @Then("User click to View Larger Map link, user can reach related page.")
+    public void userClickToViewLargerMapLinkUserCanReachRelatedPage() {
+    }
+
+    @Then("User click to satellite screen box, map should change.")
+    public void userClickToSatelliteScreenBoxMapShouldChange() {
+    }
+
+    @Then("User click to park or forest area name, user can see a label related area.")
+    public void userClickToParkOrForestAreaNameUserCanSeeALabelRelatedArea() {
+    }
+}
---
+++ src/test/resources/features/uiFeatures/AccountHubPage/us_056_googleMapFunctionality.feature
@@ -0,0 +1,10 @@
+Feature: US_056 TC_001 User should click map zoom buttons
+
+	@TEST_BU-199
+	Scenario: US_056 TC_001 User should click map zoom buttons
+		When User click to Address button, user can see related page
+		And Zoom buttons should be clickable.
+		Then User click to View Larger Map link, user can reach related page.
+		Then User click to satellite screen box, map should change.
+		Then User click to park or forest area name, user can see a label related area.
+		
