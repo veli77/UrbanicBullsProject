5b5393628c5db4137b6d68f895a34a8a4fe2bb19
gguder <gudertesteur@gmail.com>
2024-02-23 00:46:04 +0100

    US_066_SearchSellers Functionality of Several Filters
    Gokhan

--- src/test/java/stepDefinitions/UI_StepDefs/WelcomePage/US_066_SearchSellers.java
+++ src/test/java/stepDefinitions/UI_StepDefs/WelcomePage/US_066_SearchSellers.java
@@ -1,22 +1,9 @@
 package stepDefinitions.UI_StepDefs.WelcomePage;
-
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import pages.CommonPage;
-import utilities.JSUtils;
-import utilities.ReusableMethods;
-
-import java.util.ArrayList;
 import java.util.List;
 
-import static stepDefinitions.Hooks.driver;
 
 public class US_066_SearchSellers extends CommonPage {
 
