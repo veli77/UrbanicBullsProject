4464791049710923eaeb58f63efe6558fc50249f
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2024-02-22 17:18:46 +0100

    US41

--- src/test/java/stepDefinitions/UI_StepDefs/SellShareTradePage/US_041_TheButtons_stepDefs.java
+++ src/test/java/stepDefinitions/UI_StepDefs/SellShareTradePage/US_041_TheButtons_stepDefs.java
@@ -19,32 +19,32 @@ public class US_041_TheButtons_stepDefs extends CommonPage {
     }
     @Then("The Contact Us button appears and is clicked and the page opens.")
     public void the_contact_us_button_appears_and_is_clicked_and_the_page_opens() {
-        assertTrue(getSellShareTradePage().ContactUsBtn_SellShareTread.isDisplayed());
         ReusableMethods.waitFor(2);
+        assertTrue(getSellShareTradePage().ContactUsBtn_SellShareTread.isDisplayed());
         assertTrue(getSellShareTradePage().ContactUsBtn_SellShareTread.isEnabled());
     }
     @Then("The User button appears and is clicked and the page opens.")
     public void the_user_button_appears_and_is_clicked_and_the_page_opens() {
-        assertTrue(getSellShareTradePage().usernameBtn.isDisplayed());
         ReusableMethods.waitFor(2);
+        assertTrue(getSellShareTradePage().usernameBtn.isDisplayed());
         assertTrue(getSellShareTradePage().usernameBtn.isEnabled());
     }
     @Then("Logout Button appears and is clicked and the page opens")
     public void logout_button_appears_and_is_clicked_and_the_page_opens() {
-        assertTrue(getSellShareTradePage().LogoutBtn_SellShareTradePage.isDisplayed());
         ReusableMethods.waitFor(2);
+        assertTrue(getSellShareTradePage().LogoutBtn_SellShareTradePage.isDisplayed());
         assertTrue(getSellShareTradePage().LogoutBtn_SellShareTradePage.isEnabled());
     }
     @Then("The notification button appears and is clicked and the page opens.")
     public void the_notification_button_appears_and_is_clicked_and_the_page_opens() {
-        assertTrue(getSellShareTradePage().Notifications_SellShareTradePage.isDisplayed());
         ReusableMethods.waitFor(2);
+        assertTrue(getSellShareTradePage().Notifications_SellShareTradePage.isDisplayed());
         assertTrue(getSellShareTradePage().Notifications_SellShareTradePage.isEnabled());
     }
     @Then("The card appears and is clicked and the page opens.")
     public void the_card_appears_and_is_clicked_and_the_page_opens() {
-       assertTrue(getSellShareTradePage().Cart_SellShareTradePage.isDisplayed());
         ReusableMethods.waitFor(2);
+       assertTrue(getSellShareTradePage().Cart_SellShareTradePage.isDisplayed());
         assertTrue(getSellShareTradePage().Cart_SellShareTradePage.isEnabled());
     }
 
