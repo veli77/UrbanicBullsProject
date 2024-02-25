be749c0e4b4b717fee33d5e1b0cde2a846792b84
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2024-02-25 14:22:10 +0100

    US70 TC002

--- src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_42_GoToUserAccountPage_stepDefs.java
+++ src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_42_GoToUserAccountPage_stepDefs.java
@@ -26,8 +26,9 @@ public class US_42_GoToUserAccountPage_stepDefs extends CommonPage{
 
         List<String> tabs = new ArrayList<>();
         for (int i = 0; i <getAccountHomePage().accountPageLeftsidesTabs.size(); i++) {
-            ReusableMethods.hover(getAccountHomePage().accountPageLeftsidesTabs.get(i));
-
+            Thread.sleep(1000);
+            //ReusableMethods.hover(getAccountHomePage().accountPageLeftsidesTabs.get(i));
+            System.out.print(getAccountHomePage().accountPageLeftsidesTabs.get(i).getText());
             tabs.add(getAccountHomePage().accountPageLeftsidesTabs.get(i).getText());
         }
 
