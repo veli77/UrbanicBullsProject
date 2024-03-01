6e0dd1ee50fb3bc820f08acefbf52b4d8383c0a3
ZaferD <test01012018@gmail.com>
2024-03-01 23:21:32 +0100

    01.03.2024

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -3,13 +3,11 @@ package utilities;
 import static stepDefinitions.Hooks.actions;
 import static stepDefinitions.Hooks.driver;
 
-import manifold.ext.rt.api.This;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
-
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
@@ -360,13 +358,11 @@ public class ReusableMethods {
 
     public static Random random = new Random();
     public static StringBuilder sb = new StringBuilder();
-    public  static String text = "abcdefghijklmnopqrstuvwxyz";
+    public static String text = "abcdefghijklmnopqrstuvwxyz";
 
     // Random text olusturma
 
     public static String randomText(int textSize) {
-
-
 
 
         for (int i = 0; i < textSize; i++) {
@@ -379,30 +375,33 @@ public class ReusableMethods {
 
     // Random email olusturma
 
-    public static String randomEmail(int initialTextSize){
+    public static String randomEmail(int initialTextSize) {
         for (int i = 0; i < initialTextSize; i++) {
             char randomChar = text.charAt(random.nextInt(text.length()));
             sb.append(randomChar);
         }
 
-        return sb.toString()+"@gmail.com";
+        return sb.toString() + "@gmail.com";
 
     }
 
-    public static void clearAndSend( WebElement webElement, String str) {
+    public static void clearAndSend(WebElement webElement, String str) {
         webElement.clear();
         webElement.sendKeys(str);
 
     }
-    public static void selectByVisibleText( WebElement webElement,String text){
+
+    public static void selectByVisibleText(WebElement webElement, String text) {
         Select select = new Select(webElement);
         select.selectByVisibleText(text);
     }
-    public static void selectByValue(WebElement webElement,String value){
+
+    public static void selectByValue(WebElement webElement, String value) {
         Select select = new Select(webElement);
         select.selectByValue(value);
 
     }
+
     public static void selectByIndex(WebElement webElement, int index) {
         Select select = new Select(webElement);
         select.selectByIndex(index);
