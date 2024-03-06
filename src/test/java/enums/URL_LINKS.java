715bcb7115104d747c4f8009bc7d962ae280f18d
ZaferD <test01012018@gmail.com>
2024-03-06 21:09:25 +0100

    06.03.2024

--- src/test/java/enums/URL_LINKS.java
+++ src/test/java/enums/URL_LINKS.java
@@ -8,13 +8,12 @@ public enum URL_LINKS {
     CONTACTUSPAGE_URL("https://test.urbanicfarm.com/api/public/contact"),
     BASEPAGELOGIN_URL("https://urbanicfarm.com/auth/login"),
     ADDADDRESSPAGE_URL("https://test.urbanicfarm.com/api/account/address/addAddress"),
-    ADDADDRESSPAGE_URL("https://test.urbanicfarm.com/api/account/address/addAddress"),
     CANLILOGINAPIURL("https://urbanicfarm.com/api/public/login");
 
 
     private final String getLink;
 
-  URL_LINKS(String getLink) {
+    URL_LINKS(String getLink) {
         this.getLink = getLink;
     }
 
