c7e4a3973caa1caa7cdea1e4d902c9d73ecd499a
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2024-03-06 23:22:10 +0100

    US71

--- src/test/java/pages/BasketPage.java
+++ src/test/java/pages/BasketPage.java
@@ -1,4 +1,25 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BasketPage extends CommonPage{
+    @FindBy(xpath = "//button[@class='collapsible_toggle__1oudq']")
+    public WebElement enterPromocode;
+    @FindBy(xpath = "//input[@type='text']")
+    public WebElement promocodeGonder;
+    @FindBy(xpath = "//button[@class='applyPromoCode_btn__2dNcS btn']")
+    public WebElement applyButton;
+    @FindBy(xpath = "//td[@class='text-end py-3'])[3]")
+    public WebElement totalAmount;
+    @FindBy(xpath = "//div[@class='Toastify__toast-body toastr_custom-toastr__iiU37']")
+    public WebElement invalidPromocodeMessage;
+    @FindBy(xpath = "...................................")
+    public WebElement indirimliTotalHesap;
+
+
+
+
+
+
 }
--- src/test/java/stepDefinitions/UI_StepDefs/BasketPage/US71_StepDefs.java
+++ src/test/java/stepDefinitions/UI_StepDefs/BasketPage/US71_StepDefs.java
@@ -3,47 +3,53 @@ package stepDefinitions.UI_StepDefs.BasketPage;
 import io.cucumber.java.en.*;
 import pages.CommonPage;
 
+import static org.junit.Assert.*;
+
 public class US71_StepDefs extends CommonPage {
     @Then("Promocode menu should be clickable")
     public void promocode_menu_should_be_clickable() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertTrue(getBasketPage().enterPromocode.isDisplayed());
+        assertTrue(getBasketPage().enterPromocode.isEnabled());
     }
 
     @Then("User should be enter promocode")
-    public void user_should_be_enter_promocode() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_should_be_enter_promocode() throws InterruptedException {
+     Thread.sleep(2000);
+     getBasketPage().enterPromocode.click();
+     getBasketPage().promocodeGonder.sendKeys("dolar10");
+     getBasketPage().applyButton.click();
     }
 
     @Then("After entering the Promo Code, you should receive the same discount")
     public void after_entering_the_promo_code_you_should_receive_the_same_discount() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String totalAmount = getBasketPage().totalAmount.getText();
     }
 
     @Then("Promocode is entered greater than the total amount and an error message is expected.")
     public void promocode_is_entered_greater_than_the_total_amount_and_an_error_message_is_expected() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertEquals(getBasketPage().totalAmount.getText(), getBasketPage().enterPromocode.getText());
+        assertTrue(getBasketPage().invalidPromocodeMessage.isDisplayed());
     }
 
     @Then("Promocode is entered equal to the total amount and an error message is expected.")
     public void promocode_is_entered_equal_to_the_total_amount_and_an_error_message_is_expected() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertEquals(getBasketPage().totalAmount.getText(), "dolar10");
+        assertTrue(getBasketPage().invalidPromocodeMessage.isDisplayed());
     }
 
     @Then("Undefined promocode is entered and an error message is expected.")
     public void undefined_promocode_is_entered_and_an_error_message_is_expected() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       getBasketPage().enterPromocode.sendKeys("jfjfjfj");
+        assertTrue(getBasketPage().invalidPromocodeMessage.isDisplayed());
     }
 
     @Then("Promocode is entered less than the total amount and the same discount will be seen on the total amount.")
     public void promocode_is_entered_less_than_the_total_amount_and_the_same_discount_will_be_seen_on_the_total_amount() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String totalAmount = getBasketPage().totalAmount.getText();
+        getBasketPage().promocodeGonder.sendKeys("dolar10");
+        getBasketPage().applyButton.click();
+       String indirimliTotal = getBasketPage().indirimliTotalHesap.getText();
+       assertTrue(totalAmount>indirimliTotal);
     }
 
 }
