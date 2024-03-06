4222b23df4ab5cf1297658a058d1bd99e6ded69c
emineuslu <emneulusoy@gmail.com>
2024-03-06 18:30:33 +0300

    blank cleaned

--- src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_030_FacebookBtn_stepDefs.java
+++ src/test/java/stepDefinitions/UI_StepDefs/HomePage/US_030_FacebookBtn_stepDefs.java
@@ -1,13 +1,9 @@
 package stepDefinitions.UI_StepDefs.HomePage;
-
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import pages.CommonPage;
-
 public class US_030_FacebookBtn_stepDefs extends CommonPage {
-
-
 
     @When("User click the FacebookBtn")
     public void userClickTheFacebookBtn() {
@@ -18,7 +14,4 @@ public class US_030_FacebookBtn_stepDefs extends CommonPage {
     public void userShouldBeAbleToClickFacebookIcon() {
         Assert.assertTrue(getHomePage().amIinRightPage("UrbanicFarm | San Francisco CA"));
     }
-
-
-
 }
