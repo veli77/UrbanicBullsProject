707c95ca3c5a7b4515b903446aaf9e6a0d2077fc
ZaferD <test01012018@gmail.com>
2024-03-06 21:10:46 +0100

    06.03.2024

--- src/test/java/enums/URL_LINKS.java
+++ src/test/java/enums/URL_LINKS.java
@@ -2,14 +2,11 @@ package enums;
 
 public enum URL_LINKS {
     HOMEPAGE("https://test.urbanicfarm.com/"),
-
-
     LOGIN_URL("https://test.urbanicfarm.com/auth/login"),
     CONTACTUSPAGE_URL("https://test.urbanicfarm.com/api/public/contact"),
     BASEPAGELOGIN_URL("https://urbanicfarm.com/auth/login"),
     ADDADDRESSPAGE_URL("https://test.urbanicfarm.com/api/account/address/addAddress"),
     CANLILOGINAPIURL("https://urbanicfarm.com/api/public/login");
-
 
     private final String getLink;
 
