62854ce21e1a6f067afd43cc71de5d42b89f98c3
ZaferD <test01012018@gmail.com>
2024-03-06 21:08:27 +0100

    06.03.2024

--- src/test/java/stepDefinitions/API_StepDefs/US_109_Delivery_And_Pick_Up_Settings_Availability.java
+++ src/test/java/stepDefinitions/API_StepDefs/US_109_Delivery_And_Pick_Up_Settings_Availability.java
@@ -6,18 +6,13 @@ import io.cucumber.java.en.Then;
 
 public class US_109_Delivery_And_Pick_Up_Settings_Availability {
     @Given("User connect to the {string} and change availability for buyer picks up")
-    public void userConnectToTheAndChangeAvailabilityForBuyerPicksUp(String endPoint) {
-    }
-
-    @And("User should be able to change availability for buyer picks up")
-    public void user_should_be_able_to_change_availability_for_buyer_picks_up() {
+    public void user_connect_to_the_and_change_availability_for_buyer_picks_up(String string) {
 
     }
 
     @Then("User verifies success message {int}")
-    public void user_verifies_success_message(Integer int) {
+    public void user_verifies_success_message(Integer int1) {
 
     }
-
 
 }
