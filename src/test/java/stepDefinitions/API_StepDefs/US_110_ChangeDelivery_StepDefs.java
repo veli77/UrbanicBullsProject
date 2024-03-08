3c1e759d032096dd5ddb40f2bde760aff2ad100d
balperde <bekiralperdem@gmail.com>
2024-03-08 17:51:24 +0100

    US_110 updated

--- src/test/java/stepDefinitions/API_StepDefs/US_110_ChangeDelivery_StepDefs.java
+++ src/test/java/stepDefinitions/API_StepDefs/US_110_ChangeDelivery_StepDefs.java
@@ -14,13 +14,29 @@ public class US_110_ChangeDelivery_StepDefs {
 
     @Given("user connect to {string}")
     public void userConnectTo(String url) {
+
+        String jsonBody = """
+              
+                {"availability":"",
+                "availabilityBranch":"",
+                "deliveryType":["SELLER_DELIVERY"],
+                "estimatedDeliveryTime":["60_9"],
+                "flexibleDeliveryOptions":[],
+                "freeDeliveryRange":100,
+                "maxDeliveryRange":200,
+                "minFreeDeliveryOrder":15,
+                "perMileCost":1}
+                
+                """;
+
         response = given().
                 contentType(ContentType.JSON)
-//                .body("")
                 .auth()
                 .oauth2(token)
+                .body(jsonBody)
                 .when()
-                .post(url);
+                .post(url)
+                .prettyPeek();
 
     }
 
