1d585e2a467d357feacfd18567d153de1f09f89a
VedatDaglar <vedatdaglar@protonmail.com>
2024-03-14 20:43:39 +0300

    us_107_DeleteAddress.feature is Done!👏👏👏

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -16,6 +16,7 @@ import pages.CommonPage;
 import utilities.*;
 
 import static enums.USERCREDENTIAL.USER3;
+import static enums.USERCREDENTIAL.USERVEDAT;
 import static io.restassured.RestAssured.given;
 
 
@@ -106,8 +107,8 @@ public class Hooks extends CommonPage {
         //System.out.println("Login metodu calıstı");
 
         driver.get(URL_LINKS.LOGIN_URL.getLink());
-        getLoginPage().LoginEmail.sendKeys(USERCREDENTIAL.USERVEDAT.getUsername());
-        getLoginPage().input_password.sendKeys(USERCREDENTIAL.USERVEDAT.getPassword());
+        getLoginPage().LoginEmail.sendKeys(USERVEDAT.getUsername());
+        getLoginPage().input_password.sendKeys(USERVEDAT.getPassword());
         getLoginPage().submit_button.click();
         ReusableMethods.waitForPageToLoad(5);
         getAccountHomePage().zipCodeBoxCloseButton.click();
@@ -218,6 +219,11 @@ public class Hooks extends CommonPage {
     @Before("@user3token")
     public void user3Token() {
         getToken(USER3);
+    }
+
+    @Before("@userVedatToken")
+    public void userVedatToken() {
+        getToken(USERVEDAT);
     }
 
     @Before("@tokencanli")
