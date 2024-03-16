ae68aaacb4cf80691855e0368e313f965c900787
erdalkalkan <83866679+erdalkalkan@users.noreply.github.com>
2024-03-16 15:05:25 +0100

    us_203 tamamlandı, DB methodları eklendi, dummy.txt eklendi, gitignore güncellendi

--- src/test/java/stepDefinitions/DB_StepDefs/US_203_promoCode_StepDefs.java
+++ src/test/java/stepDefinitions/DB_StepDefs/US_203_promoCode_StepDefs.java
@@ -1,6 +1,5 @@
 package stepDefinitions.DB_StepDefs;
 
-import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,23 +8,23 @@ import pojo.PromoCode.PPromoCode;
 import utilities.DBUtilities;
 import utilities.ReusableMethods;
 
-import java.io.*;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 public class US_203_promoCode_StepDefs {
 
-    String promoCodeName = ReusableMethods.createName();
-    String promoCodeID;
-    String path = System.getProperty("user.dir") + "/src/test/resources/dummy.txt";
+    private String promoCodeName;
+    private List<PPromoCode> promoCodeListPojo;
+    private int promoCodeID;
+    private String updatedPromoCodeName;
+
     @When("User create Promo Code")
     public void user_create_promo_code() {
-
         String startDate = ReusableMethods.createDateWithTime(-1, -2);
         String endDate = ReusableMethods.createDateWithTime(50, 0);
-
-        DBUtilities.createPromoCode(promoCodeName,startDate,endDate);
+        promoCodeName = ReusableMethods.createName();
+        DBUtilities.createPromoCode(promoCodeName, startDate, endDate);
     }
 
     @Then("User should be able to reach Promo Code information in the correct schema and columns in DB")
@@ -38,56 +37,60 @@ public class US_203_promoCode_StepDefs {
 //        Object actualCode = promocode.getLast().get("code");
 //        Assert.assertEquals(promoCodeName,actualCode);
 
-        List<PPromoCode> pPromoCodes = DBUtilities.getPromoCodeList_pojo();
-        for (int i = 0; i < pPromoCodes.size(); i++) {
-            System.out.println("pPromoCodes.get(i).getId() = " + pPromoCodes.get(i).getId());
+        promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
+        for (int i = 0; i < promoCodeListPojo.size(); i++) {
+            System.out.println("pPromoCodes.get(i).getId() = " + promoCodeListPojo.get(i).getId());
         }
-        String actualDiscountType = pPromoCodes.getLast().getDiscount_type();
-        Assert.assertEquals("percentage",actualDiscountType);
+        String actualDiscountType = promoCodeListPojo.getLast().getDiscount_type();
+        Assert.assertEquals("percentage", actualDiscountType);
 
-        pPromoCodes.stream().forEach(t-> System.out.println(t.getId()+" "+t.getCode()+" "+t.getDiscount()));
+        promoCodeListPojo.stream().forEach(t -> System.out.println(t.getId() + " " + t.getCode() + " " + t.getDiscount()));
     }
 
 
     @Then("User should be able to get added Promo Code ID")
     public void userShouldBeAbleToGetAddedPromoCodeID() throws IOException, SQLException {
 
-        List<PPromoCode> promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
-        String promoCodeID = String.valueOf(promoCodeListPojo.getLast().getId());
+        promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
+        promoCodeID = promoCodeListPojo.getLast().getId();
 
-
-        //dosyadaki datayı günceller
-        FileWriter file = new FileWriter(path);
-        file.write(promoCodeID);
-        file.close();
+        System.out.println("promoCodeListPojo.getLast() = " + promoCodeListPojo.getLast());
+        System.out.println("promoCodeID = " + promoCodeID);
     }
 
     @And("User update Promo Code")
-    public void userUpdatePromoCode() throws IOException {
-
-        //güncel datayı okur
-        FileReader reader = new FileReader(path);
-        BufferedReader file2 = new BufferedReader(reader);
-        promoCodeID = file2.readLine();
-        file2.close();
-
+    public void userUpdatePromoCode() throws SQLException {
         String columnName = "code";
-        String columnValue = "updatedPromoCode";
+        updatedPromoCodeName = "updated Promo Name";
+        promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
 
+        System.out.println(promoCodeListPojo.getLast().getId() + " ?????? " + promoCodeListPojo.getLast().getCode());
 
-        DBUtilities.updatePromoCode(columnName,columnValue, Integer.parseInt(promoCodeID));
+        DBUtilities.updatePromoCode(columnName, updatedPromoCodeName, promoCodeID);
+
+        promoCodeListPojo.stream()
+                .filter(t -> t.getId() == promoCodeID)
+                .forEach(t -> System.out.println(t.getId() + " " + t.getCode() + " " + t.getDiscount()));
+    }
+
+    @Then("User should be able to reach updated Promo Code")
+    public void userShouldBeAbleToReachUpdatedPromoCode() throws SQLException {
+        promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
+        System.out.println("promoCodeListPojo = " + promoCodeListPojo.getLast().getCode());
+        Assert.assertTrue(promoCodeListPojo.stream()
+                .anyMatch(t -> t.getDiscount()==30));
+
     }
 
     @And("User delete Promo Code")
-    public void userDeletePromoCode() throws IOException {
-
-        //güncel datayı okur
-        FileReader reader = new FileReader(path);
-        BufferedReader file2 = new BufferedReader(reader);
-        promoCodeID = file2.readLine();
-        file2.close();
-
-        DBUtilities.deletePromoCode(Integer.parseInt(promoCodeID));
+    public void userDeletePromoCode() {
+        DBUtilities.deletePromoCode(promoCodeID);
     }
 
+    @Then("User should be able to reach deleted Promo Code")
+    public void userShouldBeAbleToReachDeletedPromoCode() throws SQLException {
+        promoCodeListPojo = DBUtilities.getPromoCodeList_pojo();
+        Assert.assertTrue(promoCodeListPojo.stream()
+                .noneMatch(t -> t.getCode().equals(updatedPromoCodeName)));
+    }
 }
