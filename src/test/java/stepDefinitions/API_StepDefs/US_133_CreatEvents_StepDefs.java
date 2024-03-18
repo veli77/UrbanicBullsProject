725961318453620978358fcf0fef8ecd2a7645f8
BirsenSelviCoskun <birsenselvi88@gmail.com>
2024-03-18 22:35:17 +0100

    Update US_133

--- src/test/java/stepDefinitions/API_StepDefs/US_133_CreatEvents_StepDefs.java
+++ src/test/java/stepDefinitions/API_StepDefs/US_133_CreatEvents_StepDefs.java
@@ -1,20 +1,65 @@
 package stepDefinitions.API_StepDefs;
 
+import enums.USERCREDENTIAL;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+import utilities.API_Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static utilities.API_Utils.response;
 
 public class US_133_CreatEvents_StepDefs {
+    RequestSpecification requestSpecification = API_Utils.preSetUp(USERCREDENTIAL.USER2);
+    private static int event_id;
     @When("user gets the create an events at {string}")
     public void userGetsTheCreateAnEventsAt(String url) {
-        String jsonBody="""
-            {
-            "address": "California, MD, Amerika Birleşik Devletleri",
-                    "city": "California",
-                    "isDefault": true,
-                    "isSellerAddress": true,
-                    "postal": "95170",
-                    "state": "St. Mary's County",
-                    "title": "Emily's Shop"
-        }
-        """;
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("title", "test");
+        parameters.put("date", "2024-05-23T12:13");
+        parameters.put("fee", 20);
+        parameters.put("isRefundable", false);
+        parameters.put("refundableUpToDay", 0);
+        parameters.put("duration", 10);
+        parameters.put("attendeeLimit", 60);
+        parameters.put("addressId", 3573);
+        parameters.put("tac", "terms");
+        response = given()
+                .spec(requestSpecification)
+                .body(parameters)
+                .when()
+                .post(url);
+        response.prettyPrint();
+
+        JsonPath jsonPath = response.jsonPath();
+       event_id = jsonPath.getInt("event.id");
+    }
+    @When("user gets the delete an events at {string}")
+    public void userGetsTheDeleteAnEventsAt(String url) {
+
+        response = given()
+                .spec(requestSpecification)
+                .body("{\"eventId\":"+ event_id + "}")
+                .when()
+                .post(url);
+        response.prettyPrint();
+
+        response.prettyPrint();
+
+    }
+
+    @Then("user verifies success {string}")
+    public void userVerifiesSuccess(String verify) {
+
+        JsonPath jsonPath = response.jsonPath();
+        String actuallValue = String.valueOf(jsonPath.getBoolean("success"));
+        Assert.assertEquals(verify,actuallValue);
+
     }
 }
--- src/test/resources/features/apiFeatures/us_133_CreatEvents.feature
+++ src/test/resources/features/apiFeatures/us_133_CreatEvents.feature
@@ -1,4 +1,9 @@
 Feature: TE of US_133
+
   Scenario: TC_01 US_133 create an event
     When user gets the create an events at "/account/event/create"
-    Then user verifies success message
+    Then user verifies success "true"
+
+  Scenario: TC_02 US_133 Delete
+    When user gets the delete an events at "/account/event/delete"
+    Then user verifies success "true"
