4aa4c0c9e66e558ca9d912a47f59b4303a5f99af
abraham907 <ersinibrahimoral@gmail.com>
2024-03-19 16:16:14 +0100

    us_139 is created and completed

--- src/test/java/stepDefinitions/API_StepDefs/US_116_AddAndDeleteProduct_stepDefs.java
+++ src/test/java/stepDefinitions/API_StepDefs/US_116_AddAndDeleteProduct_stepDefs.java
@@ -4,17 +4,14 @@ import enums.USERCREDENTIAL;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.path.json.JsonPath;
-
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-
 import utilities.API_Utils;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static stepDefinitions.Hooks.token;
 import static utilities.API_Utils.response;
 
 
