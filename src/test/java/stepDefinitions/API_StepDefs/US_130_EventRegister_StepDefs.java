5f3482af4f5274c64f6d738d10d8a0a1a4764f8f
balperde <bekiralperdem@gmail.com>
2024-03-19 03:29:58 +0100

    US_130 updated

--- src/test/java/stepDefinitions/API_StepDefs/US_130_EventRegister_StepDefs.java
+++ src/test/java/stepDefinitions/API_StepDefs/US_130_EventRegister_StepDefs.java
@@ -1,9 +1,14 @@
 package stepDefinitions.API_StepDefs;
 
+import enums.USERCREDENTIAL;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+import utilities.API_Utils;
 
 import static stepDefinitions.Hooks.response;
 import static io.restassured.RestAssured.given;
@@ -11,23 +16,42 @@ import static stepDefinitions.Hooks.token;
 
 public class US_130_EventRegister_StepDefs {
 
+
+
     @Given("user connect to  {string}")
     public void userConnectTo(String url) {
-        response = given()
-                .contentType(ContentType.JSON)
-                .auth()
-                .oauth2(token)
-                .when()
+
+        RequestSpecification requestSpecification = API_Utils.preSetUp(USERCREDENTIAL.USER2);
+
+        String jsonBody = """
+            {
+                "eventId": "990",
+                "numberOfAttendees": "1",
+                "isAuthenticatedUser": true
+            }
+            """;
+
+        API_Utils.response = given()
+                .spec(requestSpecification)
+                .body(jsonBody)
                 .post(url)
-                .prettyPeek();
+        ;
+
+       API_Utils.response.prettyPrint();
     }
 
-    @Then("user verifies event register status code {int}")
-    public void userVerifiesEventRegisterStatusCode(Integer int1) {
-        Integer actualstatusCode = response.getStatusCode();
-        Assert.assertEquals(actualstatusCode, int1);
 
+    @Then("Status code is 200")
+    public void statusCodeIs200() {
+        Assert.assertEquals("Status code is not 200", 200, API_Utils.response.getStatusCode());
     }
 
+    @And("description is successful")
+    public void descriptionIsSuccessful() {
+        String jsonResponse = API_Utils.response.getBody().asString();
+        JsonPath jsonPath = new JsonPath(jsonResponse);
+
+        Assert.assertTrue("Success value is not true", jsonPath.getBoolean("success"));
+        Assert.assertEquals("Description is not 'Waiting for Paymentt'", "Waiting for Paymentt", jsonPath.getString("descr"));
+    }
 }
-
--- src/test/resources/features/apiFeatures/us_130_EventRegister.feature
+++ src/test/resources/features/apiFeatures/us_130_EventRegister.feature
@@ -1,6 +1,7 @@
 Feature: TE of US_130  As a user I should be able to register an existing event
 
-  @userUrbanic2Token
+
   Scenario: TC_01 US_130 user should be able to register an existing event
     Given user connect to  "https://test.urbanicfarm.com/api/account/event/register"
-    Then user verifies event register status code 200
+    Then Status code is 200
+    And description is successful
